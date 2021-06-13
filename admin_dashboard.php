4bb8801ec0093f0290dbc8f74a2931dbfd129120
roshni5610 <84753022+roshni5610@users.noreply.github.com>
2021-06-13 09:53:24 +0530

    Add files via upload

---
+++ admin_dashboard.php
@@ -0,0 +1,52 @@
+
+<html>
+<head>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!----bootstrap -->
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+     <!---google fornts----->
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+ <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+ <!--custom css-->
+ <link rel="stylesheet"href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+       <!---Navbar-->
+     <header>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <a class="navbar-brand" href="#">Go for vote</a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+           <div class="navbar-nav">
+            <a class="nav-link active" href="index.html">Home <span class="sr-only"></span></a>
+             <a class="nav-link" href="admin_dashboard.php">Dashboard</a>
+             <a class="nav-link" href="user_suggestion.php">Suggestions</a>
+             <a class="nav-link" href="show_contact.php">contact</a>
+			  <a class="nav-link" href="user_details_year.php">users</a>
+			   <a class="nav-link" href="user_cand_year.php">candidates</a>
+			    <a class="nav-link" href="logout.php">Logout</a>
+           </div
+         </div>
+       </nav>
+      </header>
+      <!--navbar -->
+	  <section>
+	     <div class="container">
+		     <div class="row">
+			      <div class="col-md-12">
+				     <h1 style="margin-top:15px;">ADMIN Dashboard</h1>
+					</div>
+					</div>
+					
+					</div>
+					</section>
+					
+					  
+   
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!---google fornts----->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link rel="stylesheet"href="css/style.css">
+<!--custom css-->
+
+
+
+
+</head>
+
+ <body>
+     <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href=" "> go and vote</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" href="index.html">Home <span class="sr-only"></span></a>
+            <a class="nav-link" href="year.php">candidate</a>
+            <a class="nav-link" href="result.php">Result</a>
+            <a class="nav-link" href="about.php"">About</a>
+          </div>
+        </div>
+      </nav>
+     </header>
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>    
+    <script src="js/bootstrap.min.js"></script>  
+</body>
+</html>
